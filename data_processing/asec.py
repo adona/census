9dcce6a0e0dbd685760f8c6bbf3bad43512b3986
Adona Iosif <adona.iosif@gmail.com>
2018-10-30 16:27:12 -0400

    Add exploration of poverty demographics based on ASEC data.

--- data_processing/asec.py
+++ data_processing/asec.py
@@ -23,18 +23,32 @@ def load_and_preprocess_asec_data(filepath_data, filepath_dictionary):
     
     # Convert int and float variables
     log("Converting int and float variables.. ")
-    int_vars = ["INCTOT", "AGE", "UHRSWORKT"]
+    int_vars = ["AGE", "UHRSWORKT", "UHRSWORKLY", "WKSWORK1", "PTWEEKS", "INCTOT", "INCWAGE", "INCBUS", "INCFARM"]
     float_vars = ["ASECWT", "SPMTOTRES", "SPMTHRESH"]
-    # If replicate weights are included in the dataset, convert them as well
+    # Also convert the replicate weights
     for rw in ["REPWT", "REPWTP"]:
-        if(rw+"1" in data[0]):
-            float_vars += [rw+str(i) for i in range(1,161,1)]
+        float_vars += [rw+str(i) for i in range(1,161,1)]
     for p in data: 
         for int_var in int_vars:
-            p[int_var] = int(p[int_var])
+            if(int_var in p):
+                p[int_var] = int(p[int_var])
         for float_var in float_vars:
-            p[float_var] = float(p[float_var])
+            if(float_var in p):
+                p[float_var] = float(p[float_var])
     
+    # Annotate data with industry
+    # Load hierarchical occupations dictionary
+    filepath_occ = DATADIR + "dictionaries/occ_hierarchical.json"
+    occ_hierarchical = load_JSON(filepath_occ)
+    jobs_to_industries = {}
+    for industry in occ_hierarchical:
+        for job in occ_hierarchical[industry]:
+            jobs_to_industries[job] = industry
+    jobs_to_industries["0"] = "N/A (not applicable)"
+    for p in data:
+        p["INDLY"] = jobs_to_industries[p["OCCLY"]]
+
+
     # Annotate data with % of poverty line
     log("Annotating data with % of poverty line..")
     for p in data:
@@ -113,9 +127,100 @@ def explore_variable_work_schedule(data):
     ax.invert_yaxis()
     ax.set_title("Percentage of employees who report working irregular # hours / week, by industry")
 
+def explore_poverty_demographics(data):
+    # Who are the poor? 
+    us_population = 323.4*(10**6)
+    ndata = weighted_len(data, "ASECWT") # total # of persons the ASEC dataset represents
+    adj = us_population / ndata # roughly 1.5 — adjustment factor to convert ASEC estimates to US population estimates
+    adj_len = lambda x: adj * weighted_len(x, "ASECWT") # adjusted length of an array = # of US persons it represents
+
+    poor = [p for p in data if p["spm_perc"] <= 100]
+    npoor = adj_len(poor)
+    perc_poor = npoor / us_population
+    print(f"Roughly 1 in {1/perc_poor :.0f} Americans are poor. ({perc_poor :.1%})")
+    print(f"Thats roughly {npoor/(10**6) :.1f} million people.")
+
+    ### Segment the US poor population by whether or not they worked last year, 
+    # and if not why not (retired, disabled, in school full time, etc)
+    poor_segmented = {
+        "children": [p for p in poor if int(p["AGE"])<15],
+        # Adults who...
+        # ... worked for at least part of last year
+        "employed": [p for p in poor if p["WORKLY"] == "2"],
+        # ... report wanting but not being able to find work
+        "unemployed": [p for p in poor if p["WHYNWLY"] == "1"],
+        # ... report not being able to work last year primarily because.. 
+        "retired": [p for p in poor if p["WHYNWLY"] == "5"],
+        "disabled": [p for p in poor if p["WHYNWLY"] == "2"],
+        "school": [p for p in poor if p["WHYNWLY"] == "4"],
+        "family": [p for p in poor if p["WHYNWLY"] == "3"],
+        "other": [p for p in poor if p["WHYNWLY"] == "7"]
+    }
+
+    # Sanity check that I segmented the poor population exhaustively
+    total = 0
+    for segment in poor_segmented:
+        perc = adj_len(poor_segmented[segment])/npoor
+        total += perc
+        print(f"{segment}: {perc :.1%}")
+    print(f"Total: {total :.1%}")
+    assert(abs(total - 1) < 10**-6)
+
+    ### Look in more depth @ employed persons
+    poor_employed = poor_segmented["employed"]
+    npoor_employed = adj_len(poor_employed)
+
+    # How long did they work? (hours/week and weeks/year)
+    # Visualize
+    n_weeks_worked = [p["WKSWORK1"]+random.normalvariate(0,1) for p in poor_employed]
+    n_hours_worked = [p["UHRSWORKLY"]+random.normalvariate(0,1) for p in poor_employed]
+    fig, ax = plt.subplots()
+    ax.scatter(n_weeks_worked, n_hours_worked, s=2)
+    ax.set_title("Time worked last year (hours/week and weeks/year)")
+    ax.set_xlabel("# weeks/year")
+    ax.set_ylabel("# hours/week")
+    # Aggregate stats
+    timeworked = {
+        "allyear" : [p for p in poor_employed if p["WKSWORK1"] >= 50], # 55%
+        "allyear_fulltime" : [p for p in poor_employed if p["WKSWORK1"] >= 50 and p["UHRSWORKLY"] >= 35], # 37%
+        "allyear_parttime" : [p for p in poor_employed if p["WKSWORK1"] >= 50 and p["UHRSWORKLY"] < 35], # 18%
+        "partyear" : [p for p in poor_employed if p["WKSWORK1"] < 50], # 45%
+        "partyear_fulltime" : [p for p in poor_employed if p["WKSWORK1"] < 50 and p["UHRSWORKLY"] >= 35], # 21%
+        "partyear_parttime" : [p for p in poor_employed if p["WKSWORK1"] < 50 and p["UHRSWORKLY"] < 35] # 24%
+    }
+    for segment in timeworked:
+        perc = adj_len(timeworked[segment])/npoor_employed
+        print(f"{segment}: {perc :.1%}")
+
+
+    # How much $$ did they bring home?
+    allyear_fulltime = timeworked["allyear_fulltime"]
+    fig, ax = plt.subplots()
+    bins = range(0,100000,1000)
+    ax.hist([p["INCTOT"] for p in poor_employed], bins=bins, weights=[p["ASECWT"] for p in poor_employed])
+    ax.hist([p["INCTOT"] for p in allyear_fulltime], bins=bins, weights=[p["ASECWT"] for p in allyear_fulltime])
+    ax.set_xlabel("Total annual income")
+    ax.set_title("Total annual income for employed persons under the poverty line")
+    ax.legend(["All employed", "Employed entire year, full time"])
+
+    print(f'People in poverty who worked the entire year full time, brought home a median of ${weighted_median(allyear_fulltime, "INCTOT", "ASECWT") :,.0f}.')
+
+    # What industries were they in?
+    jobs = weighted_counter(poor_employed, "INDLY", "ASECWT")
+    for job in jobs:
+        job["adj_count"] = adj * job["count"]
+        print(f'{job["key"]}: ({job["perc"] :.1f}% = {job["adj_count"] :,.0f} persons)')
+
+    fig, ax = plt.subplots()
+    nbars = len(jobs)
+    ax.barh(range(nbars), [x["perc"] for x in jobs])
+    ax.set_yticks(range(nbars))
+    ax.set_yticklabels([x["key"] for x in jobs])
+    ax.invert_yaxis()
+
 ###
 
 filepath_data = DATADIR + "raw/asec16r.csv"
 filepath_dictionary = DATADIR + "dictionaries/asec16_dictionary_compact_extended.json"
 data = load_and_preprocess_asec_data(filepath_data, filepath_dictionary)
-explore_variable_work_schedule(data)
+# explore_poverty_demographics(data)
